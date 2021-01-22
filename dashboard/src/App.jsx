e06006bb3fc738005d83012a267004ae9aefb7d6
Alex Ullrich <alex.ullrich@gmail.com>
2021-01-22 17:37:59 -0500

    add ability to navigate between watchlist and signal list

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -3,6 +3,11 @@ import "./App.css";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import { MemoryRouter as Router } from "react-router";
+import { Link as RouterLink, Route, Switch } from "react-router-dom";
+import Link from "@material-ui/core/Link";
+import List from "@material-ui/core/List";
+import Watchlist from "./watch/Watchlist";
 import Signallist from "./signals/Signallist";
 
 function App() {
@@ -13,8 +18,24 @@ function App() {
           yogo ¯\_(ツ)_/¯
         </Typography>
       </Box>
-
-      <Signallist />
+      <Router>
+        <List>
+          <Typography>
+            <Link component={RouterLink} to="/watch">
+              Watchlist
+            </Link>
+          </Typography>
+          <Typography>
+            <Link component={RouterLink} to="/signals">
+              Signals
+            </Link>
+          </Typography>
+        </List>
+        <Switch>
+          <Route path="/watch" component={Watchlist} />
+          <Route path="/signals" component={Signallist} />
+        </Switch>
+      </Router>
     </Container>
   );
 }
