81bc34a2cc749135144983771a5542c9f07d46e8
Alex Ullrich <alex.ullrich@gmail.com>
2021-04-09 14:09:16 -0400

    simple inline editor for signals

--- web/dashboard/src/signals/Summary.jsx
+++ web/dashboard/src/signals/Summary.jsx
@@ -5,9 +5,34 @@ import Typography from "@material-ui/core/Typography";
 import { Link as RouterLink } from "react-router-dom";
 import Link from "@material-ui/core/Link";
 import PropTypes from "prop-types";
+import EdiText from "react-editext";
 
 export default function Summary({ signal }) {
+  const onSave = (val) => {
+    const signalUrl = new URL(
+      `api/signal`,
+      process.env.REACT_APP_SIGNAL_API_URL
+    );
+
+    const s = {
+      name: signal.name,
+      source: val,
+    };
+
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(s),
+    };
+
+    fetch(signalUrl, requestOptions).then((response) => {
+      // eslint-disable-next-line
+      console.log("Saved Value -> ", requestOptions.body, response);
+    });
+  };
+
   const sigpath = `/signals/${signal.name}/detail`;
+
   return (
     <Card>
       <CardContent>
@@ -17,9 +42,12 @@ export default function Summary({ signal }) {
         <Typography>
           hits: <strong>{signal.count}</strong>
         </Typography>
-        <Typography>
-          source: <i>{signal.source}</i>
-        </Typography>
+        <EdiText
+          type="textarea"
+          value={signal.source}
+          inputProps={{ rows: 5 }}
+          onSave={onSave}
+        />
         <Link component={RouterLink} to={sigpath}>
           Details
         </Link>
